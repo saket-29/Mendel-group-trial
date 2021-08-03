3a863d070db68073aea44e5c123a301e6b6cc4d9
Rujuta Shinde <66828727+rujuta1709@users.noreply.github.com>
2021-08-03 16:35:02 +0530

    Add files via upload
    Python script with my name, email, slack username, and biostack.

--- Rujuta/Rujuta.py
+++ Rujuta/Rujuta.py
@@ -1,1 +1,10 @@
+name = "Rujuta Shinde"
+email = "rujutaashinde@gmail.com"
+slack_username = "@Rujuta"
+biostack = "Computational Genomics"
+output = name + "," + email + "," + slack_username + "," + biostack
+print(output)
 
+file = open('C:/Users/Shree/Desktop/HackBio Internship/output.txt', 'w')
+file.write(output)
+file.close()
