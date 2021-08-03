4213a99c6e0b73c7c8c38c3ef72494948982625e
Shreyashi Chakravarty <88318705+Shelly0910@users.noreply.github.com>
2021-08-03 23:46:37 +0530

    Add files via upload

--- Shreyashi/shreyashi.pl
+++ Shreyashi/shreyashi.pl
@@ -1,1 +1,15 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
 
+my $str = 'Shreyashi Chakravarty, shcha1997@gmail.com, @Shreyashi, Public Health and Genomic Epidemiology'; 
+
+my $filename = 'F:\HackBio\Task 1\output.txt';
+
+open(FH, '>', $filename) or die $!;
+
+print FH $str;
+
+close(FH);
+
+print "Writing to file successfully!\n";
