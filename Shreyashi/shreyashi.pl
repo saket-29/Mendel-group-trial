5aef8ca32134d199ba6c5cfee641d20ea76fbb7d
Shreyashi Chakravarty <88318705+Shelly0910@users.noreply.github.com>
2021-08-04 13:45:36 +0530

    Update shreyashi.pl

--- Shreyashi/shreyashi.pl
+++ Shreyashi/shreyashi.pl
@@ -4,7 +4,7 @@ use strict;
 
 my $str = 'Shreyashi Chakravarty, shcha1997@gmail.com, @Shreyashi, Public Health and Genomic Epidemiology, @shellyneedswine'; 
 
-my $filename = 'F:\HackBio\Task 1\output.txt';
+my $filename = 'Ouput\output.txt';
 
 open(FH, '>', $filename) or die $!;
 
