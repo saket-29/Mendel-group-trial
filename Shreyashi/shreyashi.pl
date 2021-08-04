397a73b5e48062569ff500d4bdb44f22d867aa2c
Shreyashi Chakravarty <88318705+Shelly0910@users.noreply.github.com>
2021-08-04 12:55:46 +0530

    Update shreyashi.pl

--- Shreyashi/shreyashi.pl
+++ Shreyashi/shreyashi.pl
@@ -2,7 +2,7 @@
 use warnings;
 use strict;
 
-my $str = 'Shreyashi Chakravarty, shcha1997@gmail.com, @Shreyashi, Public Health and Genomic Epidemiology'; 
+my $str = 'Shreyashi Chakravarty, shcha1997@gmail.com, @Shreyashi, Public Health and Genomic Epidemiology, @shellyneedswine'; 
 
 my $filename = 'F:\HackBio\Task 1\output.txt';
 
