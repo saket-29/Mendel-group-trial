938b172873f641248e916fa31b52167626ddab5e
Shreyashi Chakravarty <shcha1997@gmail.com>
2021-08-04 22:14:54 +0530

    Shreyashi submission

--- Shreyashi/shreyashi.pl
+++ Shreyashi/shreyashi.pl
@@ -2,7 +2,7 @@
 use warnings;
 use strict;
 
-my $str = 'Shreyashi Chakravarty, shcha1997@gmail.com, @Shreyashi, Public Health and Genomic Epidemiology, @shellyneedswine'; 
+my $str = 'Shreyashi Chakravarty, shcha1997@gmail.com, @Shreyashi, Public Health and Genomic Epidemiology, @ShreyashiC'; 
 
 
 my $filename = 'C:\Users\Shreyashi\Documents\GitHub\Mendel-group-trial\Shreyashi\Output\output.txt';
