56c9b767469b8a244973828d495b99e1e7ce95a1
Eman2345 <88251050+Eman2345@users.noreply.github.com>
2021-08-06 00:57:37 +0200

    Delete Eman directory

--- Eman/Hackbio0.R
+++
@@ -1,9 +0,0 @@
-sink("./output.txt", append = T)
-name <- "Eman Ahmed"
-email <- "Eman.ahmed.madboli@gmail.com"
-slack_username <- "@Eman"
-biostack <- "Genomics"
-twitter_handle <- "@Emanahmed_12"
-cat(name,email,slack_username,biostack,twitter_handle, sep = ",")
-sink()
-
