382edcfbe3f61ba940717495ab42baa77e5b5881
Shreyashi Chakravarty <shcha1997@gmail.com>
2021-08-05 10:21:00 +0530

    Added Hamming Distance

--- Shreyashi/shreyashi.pl
+++ Shreyashi/shreyashi.pl
@@ -2,15 +2,23 @@
 use warnings;
 use strict;
 
-my $str = 'Shreyashi Chakravarty, shcha1997@gmail.com, @Shreyashi, Public Health and Genomic Epidemiology, @ShreyashiC'; 
+my $s1 = 'Shreyashi Chakravarty,';  
+my $s2 = 'shcha1997@gmail.com,';
+my $s3 = '@Shreyashi,';
+my $s4 = 'Public Health and Genomic Epidemiology,'; 
+my $s5 = '@ShreyaCha,'; 
+
+hd( $s3, $s5 );
+sub hd{ length( $_[ 0 ] ) - ( ( $_[ 0 ] ^ $_[ 1 ] ) =~ tr[\0][\0] ) }
 
 
-my $filename = 'C:\Users\Shreyashi\Documents\GitHub\Mendel-group-trial\Shreyashi\Output\output.txt';
-
+my $filename = 'C:\Users\Shreyashi\Documents\GitHub\Mendel-group-trial\Shreyashi\output\output.txt';
 
 open(FH, '>', $filename) or die $!;
 
-print FH $str;
+print FH $s1,$s2,$s3,$s4,$s5,
+print "", hd( $s1, $s2 );
+
 
 close(FH);
 
