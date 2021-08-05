fb15ee8c7880959273a07cbbc4e42f99d71ce93e
Tossyn12 <88286746+Tossyn12@users.noreply.github.com>
2021-08-05 22:07:24 +0100

    Add files via upload

---
+++ Tossyn/@tossyn.py
@@ -0,0 +1,10 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> name = "Adetobi Tosin"
+>>> email = "tossynemmanuel150@gmail.com"
+>>> slack_username = "@Tossyn"
+>>> twitter_username = "tossyn_emmanuel"
+>>> biostack = "Genomics"
+>>> print(name,',',email,',',slack_username,',',twitter_username,',',biostack)
+Adetobi Tosin , tossynemmanuel150@gmail.com , @Tossyn , tossyn_emmanuel , Genomics
+>>> 
