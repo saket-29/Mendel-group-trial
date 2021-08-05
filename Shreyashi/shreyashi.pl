2bc86a808d4262d76bae83f35637f76576025015
Shreyashi Chakravarty <shcha1997@gmail.com>
2021-08-05 19:53:39 +0530

    Hamming Distance Submitted

--- Shreyashi/shreyashi.pl
+++ Shreyashi/shreyashi.pl
@@ -8,16 +8,14 @@ my $s3 = '@Shreyashi,';
 my $s4 = 'Public Health and Genomic Epidemiology,'; 
 my $s5 = '@ShreyaCha,'; 
 
-hd( $s3, $s5 );
-sub hd{ length( $_[ 0 ] ) - ( ( $_[ 0 ] ^ $_[ 1 ] ) =~ tr[\0][\0] ) }
-
 
 my $filename = 'C:\Users\Shreyashi\Documents\GitHub\Mendel-group-trial\Shreyashi\output\output.txt';
 
 open(FH, '>', $filename) or die $!;
 
 print FH $s1,$s2,$s3,$s4,$s5,
-print "", hd( $s1, $s2 );
+hd($s3,$s5);
+sub hd { length( $_[ 0 ] ) - ( ( $_[ 0 ] ^ $_[ 1 ] ) =~ tr[\0][\0] ) }
 
 
 close(FH);
