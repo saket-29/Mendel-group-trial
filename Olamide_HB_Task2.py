5bd6ae5a1c1899d1f6f8a94ee33ac4b822d268ad
Olamide-21 <88270664+Olamide-21@users.noreply.github.com>
2021-08-05 12:52:42 +0100

    Add files via upload

---
+++ Olamide_HB_Task2.py
@@ -0,0 +1,19 @@
+
+name = "Olamide Eunice Faloye" 
+email = "euniceolamide12@gmail.com"
+slack_username = "@Olamide"
+biostack = "Genomics"
+twitter_handle = "@Ola_Sheila"
+
+output = name + ', ' + email + ', ' + slack_username + ', ' + biostack + ', ' + twitter_handle
+
+print(output)
+
+
+
+
+		
+
+
+
+
