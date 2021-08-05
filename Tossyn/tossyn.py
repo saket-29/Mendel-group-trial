cbcee9126dac33bfd7e2dbe659b24638325dd800
Tossyn12 <88286746+Tossyn12@users.noreply.github.com>
2021-08-05 23:07:47 +0100

    Add files via upload

---
+++ Tossyn/tossyn.py
@@ -0,0 +1,10 @@
+name = "Adetobi Tosin"
+email = "tossynemmanuel150@gmail.com"
+slack_username = "@Tossyn"
+twitter_handle = "tossyn_emmanuel"
+biostack = "Genomics"
+output = name + ', ' + email + ', ' + slack_username + ', ' + twitter_handle + ', ' + biostack
+file = open('C:/Users/Tossyn/Desktop/Tossyn/output.txt', 'w')
+file.write(output)
+file.close()
+
