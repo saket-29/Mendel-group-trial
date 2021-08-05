dc5b7c32e9461e480c282c83b0fcb622b2e57459
Eman2345 <88251050+Eman2345@users.noreply.github.com>
2021-08-05 21:28:42 +0200

    Add files via upload

---
+++ Eman/Hackbio0.R
@@ -0,0 +1,9 @@
+sink("./output.txt", append = T)
+name <- "Eman Ahmed"
+email <- "Eman.ahmed.madboli@gmail.com"
+slack_username <- "@Eman"
+biostack <- "Genomics"
+twitter_handle <- "@Emanahmed_12"
+cat(name,email,slackUsername,biostack,twitter_handle, sep = ",")
+sink()
+
