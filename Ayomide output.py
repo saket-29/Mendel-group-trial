48282c9803ad8ee664a328472f4995fe4fc63b7a
Adebesin-Ayomide <88502007+Adebesin-Ayomide@users.noreply.github.com>
2021-08-07 11:14:27 +0100

    Ayomide.text.py

---
+++ Ayomide output.py
@@ -0,0 +1,10 @@
+"C:\Users\HP\PycharmProjects\AYOMIDE APP\venv\Scripts\python.exe" "C:/Users/HP/PycharmProjects/AYOMIDE APP/main.py"
+Ayomideoutput.text
+Name: Ayomide Adebesin
+Email: adebesinayomide2@gmail.com
+Slackusername: @Ayomide
+Biostack: Bioinformatics
+Twitterhandle: Adebesinayomi10
+1
+
+Process finished with exit code 0
