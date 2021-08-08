81a625765c010152ce2244ad359bc327d28f6f59
ayshasaniya <88335136+ayshasaniya@users.noreply.github.com>
2021-08-08 20:37:46 +0530

    Add files via upload

---
+++ AYSHA/output.py
@@ -0,0 +1,10 @@
+name="Aysha"
+email="saniyarcot123@gmail.com"
+slackid="@aysha"
+biostack="transcriptomics"
+twitterid="@aysha_saniya"
+output= name + "," + email + "," + slackid + "," + biostack + "," + twitterid
+print(output)
+file = open('C:/Users/HEWLETT PACKARD/Desktop/AYSHA/output.txt', 'w')
+file.write(output)
+file.close()
